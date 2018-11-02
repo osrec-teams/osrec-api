715bd0b28a559a71be710ae72613c50ffdb9ba78
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-02 19:49:21 +0100

    Adapt eslint and jest to node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  extends: ['airbnb', 'prettier', 'prettier/react'],
+  extends: ['airbnb', 'prettier', 'prettier'],
   env: {
     es6: true,
     node: true,
     jest: true,
-    browser: true,
   },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
-  rules: {
-    'react/jsx-filename-extension': 0,
-  },
-  settings: {
-    'import/resolver': {
-      node: { moduleDirectory: ['node_modules', 'src/'] },
-    },
-  },
+  rules: {},
+  settings: {},
 };
