8069bc039a1fce93dc46146718b823f864421a13
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-05 21:24:37 +0100

    Add password hashing
    - Add bcryptjs module
    - Add create hook on User model to hash passwords

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,28 @@
 const bookshelf = require('./bookshelf.js');
+const bcrypt = require('bcryptjs');
+
+const SALT_ROUND = 10;
 
 const User = bookshelf.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
+
+  initialize: function() {
+    this.on('creating', this.hashPassword);
+  },
+
+  hashPassword: (model, attrs) => {
+    return new Promise(function(resolve, reject) {
+      bcrypt.genSalt(SALT_ROUND, function(err, salt) {
+        if (err) return reject(err);
+        bcrypt.hash(model.attributes.password, salt, function(err, hash) {
+          if (err) return reject(err);
+          model.set('password', hash);
+          resolve();
+        });
+      });
+    });
+  },
 });
 
 module.exports = User;
