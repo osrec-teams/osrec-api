14b29bdf5e9f6d8580485b4e83a2f6ca1bbba016
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-06 23:49:51 +0100

    Add test init that create the test db

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,8 @@
 export NODE_ENV=test
 export PORT=8181
 
+if ! psql -lqt -h localhost | cut -d \| -f 1 | grep -qw osrec-test; then
+  psql -h localhost postgres -c 'CREATE DATABASE "osrec-test"';
+fi
+
 jest $@
