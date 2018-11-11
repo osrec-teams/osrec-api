e26954f21cd82f3961e384c08c0ce85db9a10427
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-11 23:26:05 +0100

    Run tests in band to avoid parallel migrations

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,6 @@ if ! psql -lqt -h localhost | cut -d \| -f 1 | grep -qw osrec-test; then
   psql -h localhost postgres -c 'CREATE DATABASE "osrec-test"';
 fi
 
-jest $@
+jest -i $@
+
+psql -h localhost postgres -c 'DROP DATABASE "osrec-test"'
