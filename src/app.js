c24a4f169ed7c0d7d2f756cb4cca5e7bf0fc51d0
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-15 15:45:26 +0100

    Add cors management in API

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 const router = new Router();
@@ -9,11 +10,12 @@ const port = process.env.PORT || 8080;
 
 const users = require('./routes/users.js');
 
+app.use(cors());
 app.use(bodyParser());
 
 if (process.env.NODE_ENV === 'development') app.use(logger());
 
-router.use('/users', users.routes()), users.use(users.allowedMethods());
+router.use('/users', users.routes(), users.allowedMethods());
 
 app.use(router.routes(), router.allowedMethods());
 
