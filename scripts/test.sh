dfb796f471c79a46e6c7de83aaeb8fe363975694
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-17 11:26:26 +0100

    Fix broken test script in API

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,9 @@
 export NODE_ENV=test
 export PORT=8181
 
-if ! psql -lqt -h localhost | cut -d \| -f 1 | grep -qw osrec-test; then
-  psql -h localhost postgres -c 'CREATE DATABASE "osrec-test"';
+if psql -lqt -h localhost | cut -d \| -f 1 | grep -qw osrec-test; then
+  psql -h localhost postgres -c 'DROP DATABASE "osrec-test"' > /dev/null 2>&1 
 fi
+psql -h localhost postgres -c 'CREATE DATABASE "osrec-test"' > /dev/null 2>&1 
 
 jest -i $@
-
-psql -h localhost postgres -c 'DROP DATABASE "osrec-test"'
