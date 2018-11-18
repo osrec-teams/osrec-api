868901670a4bb3ff1335cb623657de336148073b
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-18 14:59:10 +0100

    Change migration to have unique username

--- migrations/20181102202748_users.js
+++ migrations/20181102202748_users.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function(table) {
     table.increments();
     table.string('email').unique();
-    table.string('username');
+    table.string('username').unique();
     table.string('password');
     table.timestamps();
   });
