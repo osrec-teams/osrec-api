46eba267845b0796d3ff85b610a4cd3626161b92
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-20 23:24:30 +0100

    Edit validation middleware to send back json

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -5,7 +5,7 @@ module.exports = schema => async (ctx, next) => {
     await Joi.validate(ctx.request.body, schema);
   } catch (e) {
     ctx.status = 400;
-    ctx.body = e.message;
+    ctx.body = { message: e.message };
     return;
   }
   await next();
