7289921143c675093cd8a7bf3aedc3aadbc5b192
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-21 19:31:24 +0100

    Add suppl. tests for auth route

--- src/tests/routes/auth.test.js
+++ src/tests/routes/auth.test.js
@@ -1,11 +1,17 @@
 const request = require('supertest');
 const { omit } = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const bookshelf = require('../../utils/bookshelf.js');
 
 const { knex } = bookshelf;
 
 const app = require('../../app.js');
+
+const hashPassword = async password => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
 
 beforeEach(async () => {
   await knex.migrate.latest();
@@ -20,37 +26,46 @@ afterAll(async () => {
   await knex.destroy();
 });
 
-describe('POST /users', () => {
+describe('POST /auth', async () => {
   const userData = {
-    username: 'floky',
-    password: 'floky',
-    email: 'floky@test.com',
+    username: 'frigg',
+    password: 'fr1gg',
+    email: 'frigg@asgard.com',
   };
 
-  test("route won't save user if password is not specified", async () => {
+  const loginData = { login: userData.username, password: userData.password };
+
+  test("authentication won't succeed if login is not specified", async () => {
     const res = await request(app)
       .post('/auth')
-      .send(omit(userData, 'password'));
-    expect(res.status).toEqual(401);
-    expect(res.body.message).toMatch(/Authentication failed/);
+      .send(omit(loginData, 'login'));
+    expect(res.status).toEqual(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body).toMatchObject({
+      message: 'child "login" fails because ["login" is required]',
+    });
   });
 
-  /* test("route won't save user if email is a duplicate", async () => {
-    await knex.insert(userData).into('users');
-    const res = await request(app)
-      .post('/users')
-      .send(userData);
-    expect(res.status).toEqual(400);
-    expect(res.text).toMatch(
-      /duplicate key value violates unique constraint "users_email_unique"/,
-    );
-  }); */
-
-  /* test('route will successfull connection', async () => {
+  test("authentication won't succeed if password is not specified", async () => {
     const res = await request(app)
       .post('/auth')
-      .send(userData);
-    expect(res.status).toEqual(200);
-    expect(res.body.message).toMatch(/Created/);
-   */
+      .send(omit(loginData, 'password'));
+    expect(res.status).toEqual(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body).toMatchObject({
+      message: 'child "password" fails because ["password" is required]',
+    });
+  });
+
+  // test('authentication will succeed', async () => {
+  //   await knex.insert(userData).into('users');
+  //   const res = await request(app)
+  //     .post('/auth')
+  //     .send(loginData);
+  //   expect(res.status).toEqual(200);
+  //   expect(res.type).toBe('application/json');
+  //   expect(res.body).toMatchObject({
+  //     message: 'child "password" fails because ["password" is required]',
+  //   });
+  // });
 });
